57b3128ad76001fa55af5dadcb4a904aebc71464
Nicolas E. Gimenez <nicolasgimenez@mdp.edu.ar>
2025-11-04 19:59:39 -0300

    add scrollable

--- src/components/PaymentDialog.tsx
+++ src/components/PaymentDialog.tsx
@@ -105,7 +105,7 @@ const PaymentDialog = ({ open, onOpenChange, investment }: PaymentDialogProps) =
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-[500px]">
+      <DialogContent className="sm:max-w-[500px] max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>Procesar Inversión</DialogTitle>
           <DialogDescription>
